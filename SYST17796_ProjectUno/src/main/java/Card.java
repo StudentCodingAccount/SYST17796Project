d97854c79e3a2f8fc94bbfe76b6db198df300fcb
StudentCodingAccount <97966468+StudentCodingAccount@users.noreply.github.com>
2022-04-22 22:25:22 -0400

    Add files via upload

---
+++ SYST17796_ProjectUno/src/main/java/Card.java
@@ -0,0 +1,164 @@
+
+/**
+ * SYST17796 Final Project - UNO
+ * @author Syed Ibraz Hassan Ghazi, Ethan Dao, Obada Agha
+ * March 2022
+ * @modifier Ethan Dao
+ * @modifier Syed Ibraz Hassan Ghazi
+ */
+
+/**
+ * The Card class to store the different values of the Uno cards. 
+ * @author Hassan 
+ */
+public class Card {
+	
+	private String color;
+	private int value;
+	private int specialValue;
+	private boolean special;
+
+	public Card(int value,String color) {
+		
+		/*
+		 * constructs a non speicla card
+		 * sets the color and numerical value
+		 * sets it to the normal card category
+		 */
+		
+		
+		this.color = color;
+		this.value = value;
+		this.specialValue = 0;
+		this.special = false;
+	}
+	
+	
+	public Card(int specialValue) { // constructor for special cards like +4 and +2	
+		
+		/*
+		 * assigns special value to the card
+		 * sets the card to special category
+		 */
+		
+		this.color="";
+		this.specialValue = specialValue;
+		this.value = 0;
+		this.special = true;
+	}
+	
+	public String getColor() {
+		/*
+		 * returns the colour of the card
+		 */
+		
+		return this.color;
+	}
+	
+	public int getValue() {
+		
+		/*
+		 * returns number value of the card
+		 */
+		
+		if(!this.special) {
+		return this.value;}
+		
+		else {
+			return this.specialValue;
+		}
+	}
+	
+	public String toString() {
+		
+		/*
+		 * prints the card.
+		 * prints [ color - card value ] if the card is not a special card
+		 * prints [ + value ] if it is a special card 
+		 */
+		
+//		if(!special) {
+//			return "[ "+this.color+"-"+this.value+" ]";
+//		}
+//		
+//		else if(special){
+//			
+//			return "[ "+"+"+this.specialValue+" ]"; 
+//		}
+//		return null;
+		
+		String[] card = {" ----- ","|     |","|     |"," ----- "};
+		String c = "";
+		
+		
+		for(int i=0;i<card.length;i++) {
+				
+			for(int j=0;j<1;j++) {
+					
+				if(!this.isSpecial()) {
+					if(i==1) {
+						
+						c = c +"| "+this.getColor()+" |"+" ";
+						
+					}
+					
+					else if(i==2) {
+						
+						c = c + "|  "+this.getValue()+"  |"+" ";
+					}
+					
+					else {
+						c = c + card[i]+" "; 
+					}
+					
+					}
+				
+				else if(this.isSpecial()) {
+					
+					if(i==1) {
+						
+						c = c +"| "+"+"+this.getValue()+"  |"+" ";
+						
+					}
+					else {
+						c = c + card[i]+" ";
+					}
+					
+				}
+				
+					}
+				
+				c +="\n";
+				
+			}
+				
+		
+		
+		
+		return c;
+		
+		
+		
+		
+	}
+	
+	public boolean isSpecial() {
+		
+		/*
+		 * returns true if card is a special card
+		 * returns false if card is not a special card
+		 */
+		
+		
+		if(special) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	
+	
+	
+}
---
+++ SYST17796_ProjectUno/src/main/java/Main.java
@@ -0,0 +1,26 @@
+
+/**
+ * SYST17796 Final Project - UNO
+ * A simple text based uno card game made in Java
+ * @author Syed Ibraz Hassan Ghazi, Ethan Dao, Obada Agha
+ * March 2022
+ * @modifier Ethan Dao
+ * @modifier Syed Ibraz Hassan Ghazi
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 *Main functions just runs the game by calling the game method. game mthod resides in the Uno class.
+		 */
+	
+		Uno game = new Uno();
+		
+		game.game();
+		
+		
+	}
+
+}
